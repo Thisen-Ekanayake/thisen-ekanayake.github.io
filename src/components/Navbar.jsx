f82a7eb0460a2fc84fedb41a637cc1d0b8689ae0
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-14 19:51:04 +0530

    added blog section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const navItems = [
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
+  { name: "Blog", href: "#blog" },
   { name: "Contact", href: "#contact" },
 ];
 
