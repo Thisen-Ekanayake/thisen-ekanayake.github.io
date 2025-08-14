7e146453b0e6bd0adc87807d309da426c8efbb49
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-08-14 19:51:32 +0530

    added blog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
+import { BlogSection } from "../components/BlogSection";
 
 export const Home = () => {
   return (
@@ -24,6 +25,7 @@ export const Home = () => {
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <BlogSection />
         <ContactSection />
       </main>
 
